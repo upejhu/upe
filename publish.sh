887d13a2f1da93b41d65351be241b2297a6cef31
Disa Mhembere <dmhembe1@jhu.edu>
2016-08-30 19:00:57 -0400

    publishing url change

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 python members.py
 printf "%s" "user: "
 read user
-rsync -avz --exclude '.*' --exclude '*.py' --exclude '*.sh' --exclude '*.html.template' --exclude '*.json' --exclude '*.md'  . $user@acm.jhu.edu:/afs/acm.jhu.edu/group/upe/public_html
+rsync -avz --exclude '.*' --exclude '*.py' --exclude '*.sh' --exclude '*.html.template' --exclude '*.json' --exclude '*.md'  . $user@egg.acm.jhu.edu:/afs/acm.jhu.edu/group/upe/public_html
