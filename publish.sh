55c54873cd4f677478d5990a8897ab246f346992
Alexander Wu <alexanderwu68@yahoo.com>
2021-04-29 02:03:02 -0700

    Fix bugs no need to thank me

--- publish.sh
+++
@@ -1,4 +0,0 @@
-python members.py
-printf "%s" "user: "
-read user
-rsync -avz --exclude '.*' --exclude '*.py' --exclude '*.sh' --exclude '*.html.template' --exclude '*.json' --exclude '*.md'  . $user@egg.acm.jhu.edu:/afs/acm.jhu.edu/group/upe/public_html
